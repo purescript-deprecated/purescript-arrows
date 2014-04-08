4f1021c51d37e08c24109ca3f1abeca1d8e20ad5
Gary Burgess <gary.burgess@gmail.com>
2014-04-08 21:20:33 +0100

    Update classes, rename zeroArrow

--- src/Control/Arrow.purs
+++ src/Control/Arrow.purs
@@ -2,7 +2,7 @@ module Control.Arrow where
 
 import Data.Tuple (Tuple(..), swap)
 
-class Arrow a where
+class (Category a) <= Arrow a where
   arr :: forall b c. (b -> c) -> a b c
   first :: forall b c d. a b c -> a (Tuple b d) (Tuple c d)
 
@@ -10,20 +10,20 @@ instance arrowFunction :: Arrow (->) where
   arr f = f
   first f (Tuple b d) = Tuple (f b) d
 
-second :: forall a b c d. (Category a, Arrow a) => a b c -> a (Tuple d b) (Tuple d c)
+second :: forall a b c d. (Arrow a) => a b c -> a (Tuple d b) (Tuple d c)
 second f = arr swap >>> first f >>> arr swap
 
 infixr 3 ***
 infixr 3 &&&
 
-(***) :: forall a b b' c c'. (Category a, Arrow a) => a b c -> a b' c' -> a (Tuple b b') (Tuple c c')
+(***) :: forall a b b' c c'. (Arrow a) => a b c -> a b' c' -> a (Tuple b b') (Tuple c c')
 (***) f g = first f >>> second g
 
-(&&&) :: forall a b b' c c'. (Category a, Arrow a) => a b c -> a b c' -> a b (Tuple c c')
+(&&&) :: forall a b b' c c'. (Arrow a) => a b c -> a b c' -> a b (Tuple c c')
 (&&&) f g = arr (\b -> Tuple b b) >>> (f *** g)
 
 class ArrowZero a where
-  zeroArrow :: forall b c. a b c
+  azero :: forall b c. a b c
 
 infixr 5 <+>
 
