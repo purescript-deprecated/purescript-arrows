af891c7ee47bbd9ff859a0b3c2283ea71ef1b033
Gary Burgess <gary.burgess@gmail.com>
2014-07-19 00:54:12 +0100

    Update build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,30 @@
 module.exports = function(grunt) {
 
-  "use strict";
+    "use strict";
 
-  grunt.initConfig({ 
-  
-    libFiles: [
-      "src/**/*.purs",
-      "bower_components/purescript-*/src/**/*.purs"
-    ],
-    
-    clean: {
-      lib: ["js", "externs"]
-    },
-  
-    pscMake: ["<%=libFiles%>"],
-    dotPsci: ["<%=libFiles%>"]
-    
-  });
+    grunt.initConfig({ 
 
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-purescript");
-  
-  grunt.registerTask("make", ["pscMake", "dotPsci"]);
-  grunt.registerTask("default", ["make"]);
+        libFiles: [
+          "src/**/*.purs",
+          "bower_components/purescript-*/src/**/*.purs",
+        ],
+
+        clean: ["output"],
+
+        pscMake: ["<%=libFiles%>"],
+        dotPsci: ["<%=libFiles%>"],
+        docgen: {
+            readme: {
+                src: "src/**/*.purs",
+                dest: "docs/Module.md"
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-purescript");
+
+    grunt.registerTask("make", ["pscMake", "dotPsci", "docgen"]);
+    grunt.registerTask("default", ["make"]);
 };
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 purescript-arrows
 =================
 
+[![Build Status](https://travis-ci.org/purescript-contrib/purescript-arrows.svg?branch=master)](https://travis-ci.org/purescript-contrib/purescript-arrows)
+
 Type classes for Arrows.
+
+- [Module documentation](docs/Module.md)
